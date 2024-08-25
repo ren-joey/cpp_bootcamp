06b316803af7341b25c7caf60f3d44223a7ba5c4
ren-joey <tsuyiren@gmail.com>
2024-08-25 13:43:48 +0800

    feat: 🎸 update video No. 35

---
+++ 35_getter_setter.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class member
+{
+    private:
+        int a;
+        int b;
+
+    public:
+        int get_a()
+        {
+            return a;
+        }
+        int get_b()
+        {
+            return b;
+        }
+        void set_a(int val)
+        {
+            a = val;
+        }
+        void set_b(int val)
+        {
+            b = val;
+        }
+};
+
+int main()
+{
+    member member1;
+    member1.set_a(10);
+    cout << member1.get_a() << endl;
+
+    return 0;
+}
